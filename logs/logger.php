7360d43936d70b7493c73b2ec4f18ca9ccc209db
SevenOperation <sevenoperation@hotmail.de>
2017-07-27 00:18:59 +0200

    Merge origin/master
    Conflicts:
    	ControlInterface/pipes.txt

--- logs/logger.php
+++ logs/logger.php
@@ -1,5 +1,5 @@
 <?php
 
 function logIP(){
-    file_put_contents('logs/user.txt', 'IP: ' . $_SERVER['REMOTE_ADDR'] . " | OS: " . $_SERVER["HTTP_USER_AGENT"] . " | Datum: ".date("d.m.Y"). " | Zeit: " .date("H:i:sa") . "\n", FILE_APPEND ); 
+    file_put_contents('user.txt', 'IP: ' . $_SERVER['REMOTE_ADDR'] . " | OS: " . $_SERVER["HTTP_USER_AGENT"] . " | Datum: ".date("d.m.Y"). " | Zeit: " .date("H:i:sa") . "\n", FILE_APPEND ); 
 }
