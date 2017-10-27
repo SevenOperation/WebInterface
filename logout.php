e1352453ff13bd3d2b3e20fd7a1196cfe91b5a5b
SevenOperation <mo.fernitz@hotmail.de>
2017-10-27 10:01:29 +0200

    logout.php now redirects to index.php

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 setcookie("benutzerdaten", NULL);
+header('Location: /index.php');
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
