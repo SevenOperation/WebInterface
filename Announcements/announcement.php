a83afe4353ede589cc59ab3e5d77832a70e16411
SevenOperation <sevenoperation@hotmail.de>
2017-10-28 00:39:14 +0200

    Merge origin/master Better visual Conflicts: 	ControlInterface/pipes.txt

--- Announcements/announcement.php
+++ Announcements/announcement.php
@@ -12,7 +12,7 @@ $db = new PDO('mysql:host=localhost;dbname=news', 'root', '');
 $announcements = $db->query("Select titel , content from announcement");
 if(isset($announcements)){
 foreach( $announcements as $announcement ){
- echo "<div style='padding: 1%; background-color: darkblue'><div style='padding-top: 2%; padding-bottom: 2%; margin: auto; width: 25%'>".
+ echo "<div style='padding: 1%; background-color: darkblue; width: 25%:w'><div style='padding-top: 2%; padding-bottom: 2%; margin: auto; width: 100%'>".
       "<div style='background-color: white'><p style='width: 100%; text-align: center'>".$announcement['titel']."</p></div>".
       "<div style='background-color: white; height: 500px;'><p style='width: 100%;'>".nl2br($announcement['content'])."</p></div></div></div>";
 }
