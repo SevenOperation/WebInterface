047ae5b8c7118d2ebcc6218cff1fc33d0b322537
SevenOperation <mo.fernitz@hotmail.de>
2017-10-27 15:46:09 +0200

    Dies das ananas habe ein bisschen was auf der news Seite verändert

--- News/news.php
+++ News/news.php
@@ -45,50 +45,12 @@ if (isset($_COOKIE['benutzerdaten'])) {
             }
         </script>
     </head>
-    <body style='background-image: url(/logo_1.png);  background-size: cover;'>
-        <div style='background-color: #24292e; padding-top: 12px; padding-bottom: 12px; line-height: 1.5 ;'>
-            <div class='head' style='width: 960px; margin-left: auto; margin-right: auto; line-height: 1.5; font-size: 14px'>
-                <ul style='margin-top: 0; list-style: none; float: left; padding-left: 0; margin-bottom: 0'>
-                    <li><a href='/index.php'>Startseite</a></li>
-                    <li><a href='/News/news.php'>News</a></li>
-                    <li><a href='/games.php'>Games</a></li>
-                    <li><a href=''>Ankündigungen</a></li>
-                    <?php
-                    if (isset($_COOKIE['benutzerdaten']) && isset($_SESSION["angemeldet" . $username]) && $_SESSION["angemeldet" . $username] == true) {
-                        echo "</ul><ul style='margin: 0; list-style: none; float: right;'>
-                    <li style='float: left'><a class='einloggenCss' onclick='userMenue()'>" . $username . "</a></li>
-                    </ul></div></div>";
-                        echo "<div style='width: 1080px; margin-left: auto; margin-right: auto; padding: 0;'>
-                              <div style='position: absolute; z-index:2' id='login' class='drop2' align='center'>
-                              <p><a style='color:black;' href='ControlInterface/controlinterface.php'>WebInterface</a></p>
-                              <p><a style='color:black;' href='/logout.php'>LogOut</a></p>
-                              </div></div>";
-                    } else {
-                        echo "</ul><ul style='margin: 0; list-style: none; float: right;'>
-                    <li style='float: left'><a class='einloggenCss' onclick='userMenue()'>Einloggen</a></li>
-                    <li style='float: left'><a class='einloggenCss' onclick='registerUser()'>Registrieren</a></li>
-                    </ul></div></div>";
-                        echo "<div style='width: 960px; margin-left: auto; margin-right: auto;'>
-                        <div id='login' style='width: 340px;' class='drop2' align='center'>
-                            <form action='/ControlInterface/datenueberpruefung.php' method='post'>
-                                <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
-                                <p><input class='input' id='password' name='password' type='text' placeholder='Password'/></p>
-                                <p><button type='submit'>Einloggen</button></p>
-                            </form>
-                        </div>
-                    </div>
-                    <div id='register' style='width: 340px;' class='drop2' align='center'>
-                        <form action='/ControlInterface/registrieren_datenueberpruefung.php' method='post'>
-                            <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
-                            <p><input class='input' id='password' name='password' type='text' placeholder='Password'/></p>
-                            <p><input class='input' id='passwordw' name='passwordw' type='text' placeholder='Password Wiederholen' required='required'/></p>
-                            <p><button type='submit'>Registrieren</button>
-                            </p>
-                        </form>
-                    </div>";
-                    }
-                    ?>
-                   <iframe style="margin-left: -1%; position: absolute; height: 100%; border: none; width: 100% ; margin-top: -10px;"  src="http://www.youtube.com/embed?listType=user_uploads&list=We Are The Gamers" allowfullscreen scrolling="no" ></iframe>
+<?php
+require_once '../testAdminContentInterface/htmlbuildHelper.php';
+getNormalBodyTop($username);
+?>
+<iframe width="600" height="340" src="https://www.youtube.com/embed/latest?list=UChX1P_mHNWCcaa9oHvHiRAg" frameborder="0" allowfullscreen></iframe> 
+<iframe width="600" height="340" src="https://www.youtube.com/channel/UChX1P_mHNWCcaa9oHvHiRAg/feed?activity_view=1" frameborder="0" allowfullscreen></iframe>
                     </body>
                     </html>
 <?php
