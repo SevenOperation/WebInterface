16bb93a1031d757f8d61cbfc3de5900e0e0f0976
SevenOperation <mo.fernitz@hotmail.de>
2017-11-14 17:09:49 +0100

    prepairing for comments

--- Announcements/announcement.php
+++ Announcements/announcement.php
@@ -6,6 +6,7 @@ if (isset($_COOKIE['benutzerdaten'])) {
     $username = explode("-", $_COOKIE['benutzerdaten'])[0];
     $password = explode("-", $_COOKIE['benutzerdaten'])[1];
 }
+
 getNormalHeader();
 getNormalBodyTop($username);
 $db = new PDO('mysql:host=localhost;dbname=news', 'root', '');
@@ -15,7 +16,18 @@ foreach( $announcements as $announcement ){
  echo "<div style='width: 52%; padding: 2%; margin: auto'><div style='padding: 2%; margin: auto; width: 100%; background-color: darkblue'>".
       "<div style='background-color: white'><p style='width: 100%; text-align: center'>".$announcement['titel']."</p></div>".
       "<div style='background-color: white;'><p style='width: 100%;'>".nl2br($announcement['content'])."</p></div>";
- echo "<button name='".$announcement['id']."' onclick='showComments()'>Show Comments</button></div></div>";
+ echo "<button name='".$announcement['id']."' onclick='showComments('".$announcement['id']."')'>Show Comments</button></div></div>";
 }
 }
 ?>
+<script type="text/javascript">
+function showComments(id){
+    var request = new XMLHttpRequest()
+     request.onreadystatechange = function() { 
+        if (request.readyState == 4 && request.status == 200)
+            callback(request.responseText);
+    }
+    request.open("GET", "getComments.php?id=" + id, true); // true for asynchronous 
+    request.send(null);
+    }
+</script>
