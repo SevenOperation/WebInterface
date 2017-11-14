9595b361866d13e7a396378fa93527f6bc46b860
SevenOperation <mo.fernitz@hotmail.de>
2017-11-14 13:17:22 +0100

    better visual

--- testAdminContentInterface/htmlbuildHelper.php
+++ testAdminContentInterface/htmlbuildHelper.php
@@ -40,7 +40,7 @@ echo "<html>" .
 function getNormalBodyTop($username){
 echo "<body style='background-image: url(/Logo_1.png);  background-size: cotain;'>".
         "<div style='background-color: #24292e; padding-top: 12px; padding-bottom: 12px; line-height: 1.5 ;'>".
-            "<div class='head' style='width: 960px; margin-left: auto; margin-right: auto; line-height: 1.5; font-size: 14px'>".
+            "<div class='head' style='width: 52%; margin-left: auto; margin-right: auto; line-height: 1.5; font-size: 14px'>".
                 "<ul style='margin-top: 0; list-style: none; float: left; padding-left: 0; margin-bottom: 0'>".
                     "<li><a href='/index.php'>Startseite</a></li>".
                     "<li><a href='/News/news.php'>News</a></li>".
@@ -59,7 +59,7 @@ echo "<body style='background-image: url(/Logo_1.png);  background-size: cotain;
                         echo "</ul><ul style='margin: 0; list-style: none; float: right;'>
                     <li style='float: left'><a class='einloggenCss' onclick='userMenue()'>Einloggen</a></li>
                     <li style='float: left'><a class='einloggenCss' onclick='registerUser()'>Registrieren</a></li>
-                    </ul></div><div style='width: 82%; margin-left: auto; margin-right: auto;'>
+                    </ul></div><div style='width: 52%; margin-left: auto; margin-right: auto;'>
                         <div id='login' style='width: 340px;' class='drop2' align='center'>
                             <form action='/ControlInterface/datenueberpruefung.php' method='post'>
                                 <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
@@ -77,24 +77,6 @@ echo "<body style='background-image: url(/Logo_1.png);  background-size: cotain;
                             </p>
                         </form>
                     </div></div>";
-                        echo "<div style='width: 960px; margin-left: auto; margin-right: auto;'>
-                        <div id='login' style='width: 340px;' class='drop2' align='center'>
-                            <form action='/ControlInterface/datenueberpruefung.php' method='post'>
-                                <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
-                                <p><input class='input' id='password' name='password' type='password' placeholder='Password'/></p>
-                                <p><button type='submit'>Einloggen</button></p>
-                            </form>
-                        </div>
-                    </div>
-                    <div id='register' style='width: 340px;' class='drop2' align='center'>
-                        <form action='/ControlInterface/registrieren_datenueberpruefung.php' method='post'>
-                            <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
-                            <p><input class='input' id='password' name='password' type='password' placeholder='Password'/></p>
-                            <p><input class='input' id='passwordw' name='passwordw' type='password' placeholder='Password Wiederholen' required='required'/></p>
-                            <p><button type='submit'>Registrieren</button>
-                            </p>
-                        </form>
-                    </div>";
                     }
 }
 
