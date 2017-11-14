974d4c08420ced4ce7b0c364df3f9bdb69bed2ab
SevenOperation <sevenoperation@hotmail.de>
2017-11-14 23:52:28 +0100

    Merge origin/master prepairing for comments

--- Announcements/announcement.php
+++ Announcements/announcement.php
@@ -10,7 +10,7 @@ $extrascipt = "\r\n function showComments(id){".
     "\r\n var request = new XMLHttpRequest();".
      "\r\n request.onreadystatechange = function() {" .
         "\r\n if (request.readyState == 4 && request.status == 200)".
-            "\r\n document.getElementById(id).innerhtml = request.responseText;".
+            "\r\n document.getElementById(id).innerhtml += request.responseText;".
             "\r\n alert(request.responseText)".
     "\r\n }".
     "\r\n ".
