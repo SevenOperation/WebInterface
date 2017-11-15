3b408fbd5f3892b760ec128647c444fd687c7c66
SevenOperation <mo.fernitz@hotmail.de>
2017-11-15 13:44:12 +0100

    prepairing for comments

--- Announcements/announcement.php
+++ Announcements/announcement.php
@@ -46,7 +46,7 @@ foreach( $announcements as $announcement ){
  echo "<button name='".$announcement['id']."' onclick='showComments(".$announcement['id'].")'>Show Comments</button>".
       "<div style='background-color: white'>".
       "<p>Comment:</p>".
-      "<textarea id='".$announcement['id']."CC' maxlength='200' name='content' style='width: 100%; margin: auto; box-sizing: border-box'>".
+      "<textarea id='".$announcement['id']."CC' maxlength='100' name='content' style='width: 100%; margin: auto; box-sizing: border-box'>".
       "</textarea>".
       "<button onclick='createComment(\"".$announcement["id"]."CC,".$announcement['id'].")' >Add comment</button>".
       "</div>".
