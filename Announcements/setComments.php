647a9c3f3640cdd737b91d0af3f2c588413e30bd
SevenOperation <mo.fernitz@hotmail.de>
2017-11-15 16:41:51 +0100

    p

--- Announcements/setComments.php
+++ Announcements/setComments.php
@@ -11,9 +11,11 @@ if(isset($_GET['id']) && isset($_GET['content'])){
     $username = explode("-", $_COOKIE['benutzerdaten'])[0];
     $password = explode("-", $_COOKIE['benutzerdaten'])[1];
     $userID = $db->query("Select id FROM user WHERE password=".$password. " and username=".$username.")");
+    echo var_dump($db->errorInfo());
     }
     $db = new PDO('mysql:host=localhost;dbname=news', 'root', '');
     $db->query("INSERT into comments (userID,annoucementID,created,content) VALUES (".$userID.",".$id.",".$date.",".$contentGET.")");
+    echo var_dump($db->errorInfo());
 }else{
     
 }
