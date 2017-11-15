cf85ae217ed9199d6038d638775eb1828c54ad24
SevenOperation <mo.fernitz@hotmail.de>
2017-11-15 11:37:05 +0100

    prepairing for comments

--- Announcements/getComments.php
+++ Announcements/getComments.php
@@ -7,7 +7,8 @@ if(isset($_GET['id'])){
     $comments = $db->query("Select * from comments WHERE annoucementID=". $_GET['id'] ." ORDER BY id DESC");
     if(isset($comments)){
     foreach ($comments as $comment){
-        echo "<textarea maxlength='1000' name='content' style='width: 100%; height: 90%; margin: auto; box-sizing: border-box'>".$comment['content']."</textarea>";
+        echo "<textarea maxlength='1000' name='content' style='width: 100%; height: 90%; margin: auto; box-sizing: border-box'>".$comment['content']."</textarea>\r\n".
+              "<p>$comment['created']</p>";
     }
     }else{
         echo "<p>No Comments could be found</p>";
