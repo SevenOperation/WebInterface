84a63a3ec596a493d52974466b4a7e5a33862e63
SevenOperation <mo.fernitz@hotmail.de>
2017-11-15 15:40:24 +0100

    </script>

--- upload.php
+++ upload.php
@@ -15,7 +15,7 @@
 
 
 <?php
-$target_dir = "uploads/";
+$target_dir = "/var/www/html/WebInterface/uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
