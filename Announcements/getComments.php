0148a1ee2ea443dffdd94e1915495b6493f9462c
SevenOperation <mo.fernitz@hotmail.de>
2017-11-15 11:38:51 +0100

    prepairing for comments

--- Announcements/getComments.php
+++ Announcements/getComments.php
@@ -8,7 +8,7 @@ if(isset($_GET['id'])){
     if(isset($comments)){
     foreach ($comments as $comment){
         echo "<textarea maxlength='1000' name='content' style='width: 100%; height: 90%; margin: auto; box-sizing: border-box'>".$comment['content']."</textarea>\r\n".
-              "<p>$comment['created']</p>";
+              "<p>".$comment['created']."</p>";
     }
     }else{
         echo "<p>No Comments could be found</p>";
