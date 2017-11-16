02487121258af08fe53c1d53b330410af083e385
SevenOperation <mo.fernitz@hotmail.de>
2017-11-16 12:52:56 +0100

    p

--- Announcements/getComments.php
+++ Announcements/getComments.php
@@ -5,10 +5,12 @@
 if(isset($_GET['id'])){
     $db = new PDO('mysql:host=localhost;dbname=news', 'root', '');
     $comments = $db->query("Select * from comments WHERE annoucementID=". $_GET['id'] ." ORDER BY id DESC");
+    $db = new PDO('mysql:host=localhost;dbname=users', 'root', '');
     if($comments != FALSE && $comments->rowCount() != 0){
     foreach ($comments as $comment){
-        echo "<div name='".$comment['annoucementID']."C'style='background-color: white;'>\r\n".
-              "\r\n <p style='font-size:16px'>TestName</p>".
+        $creator = $db->query("Select username from user WHERE id =". $comments['userID'] ." ORDER BY id DESC");
+        echo "<div name='".$comment['annoucementID']->fetch(PDO::FETCH_ASSOC)[0]['username']."C'style='background-color: white;'>\r\n".
+              "\r\n <p style='font-size:16px'>$creator</p>".
               "\r\n <img src='' height='48' width='48' style='position: absolute'></img>".
               "<p style='margin-left: 50px; margin-right: 20px; font-size:16px; height: 96px; border-color: black; border-width: 1px; border-style: solid; box-sizing: border-box'>".nl2br(htmlspecialchars($comment['content']))."</p>\r\n".
               "<p style='font-size:16px; margin-top: 10px'>".$comment['created']."</p>\r\n</div>";
