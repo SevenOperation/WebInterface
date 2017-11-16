cdbe4f4c93ddaf05ffcc1eb5ca95d14f95943b02
SevenOperation <mo.fernitz@hotmail.de>
2017-11-16 12:30:40 +0100

    p

--- Announcements/getComments.php
+++ Announcements/getComments.php
@@ -7,7 +7,10 @@ if(isset($_GET['id'])){
     $comments = $db->query("Select * from comments WHERE annoucementID=". $_GET['id'] ." ORDER BY id DESC");
     if($comments != FALSE && $comments->rowCount() != 0){
     foreach ($comments as $comment){
-        echo "<div name='".$comment['annoucementID']."C'style='background-color: white;'>\r\n<p style='width: 100%;'>".nl2br(htmlspecialchars($comment['content']))."</p>\r\n".
+        echo "<div name='".$comment['annoucementID']."C'style='background-color: white;'>\r\n".
+              "\r\n <p style='font-size:16px'>TestName</p>".
+              "\r\n <img src='' height='48' width='48' style='position: absolute'></img>".
+              "<p style='width: 100%;'>".nl2br(htmlspecialchars($comment['content']))."</p>\r\n".
               "<p>".$comment['created']."</p>\r\n</div>";
     }
     }else{
