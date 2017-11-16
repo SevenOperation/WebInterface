3e89acc97c5508e155de938bcf844c479a997afe
SevenOperation <mo.fernitz@hotmail.de>
2017-11-16 23:00:46 +0100

    p

--- Announcements/getComments.php
+++ Announcements/getComments.php
@@ -9,13 +9,16 @@ if(isset($_GET['id'])){
     if($comments != FALSE && $comments->rowCount() != 0){
     foreach ($comments as $comment){
         $creator = $db->query("Select username from user WHERE id=". $comment['userID'] ." ORDER BY id DESC");
+        if(isset($creator) && !$creator){
         $creatorS = $creator->fetch(PDO::FETCH_ASSOC)['username'];
+        
         if(!isset($creatorS)) $creatorS="Unknown (Guest)";
         echo "<div name='".$comment['annoucementID']."C'style='background-color: white;'>\r\n".
               "\r\n <p style='font-size:16px'>".$creatorS."</p>".
               "\r\n <img src='' height='48' width='48' style='position: absolute'></img>".
               "<p style='margin-left: 50px; margin-right: 20px; font-size:16px; height: 96px; border-color: black; border-width: 1px; border-style: solid; box-sizing: border-box'>".nl2br(htmlspecialchars($comment['content']))."</p>\r\n".
               "<p style='font-size:16px; margin-top: 10px'>".$comment['created']."</p>\r\n</div>";
+    }
     }
     }else{
         echo "<div name='".$_GET['id']."C'style='background-color: white;'>\r\n<p style='width: 100%;'>No comments could be found</p>".
