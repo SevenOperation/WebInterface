5aab95532d8c52b3ff2c227075ba21332200e378
SevenOperation <mo.fernitz@hotmail.de>
2017-11-16 13:00:51 +0100

    p

--- Announcements/setComments.php
+++ Announcements/setComments.php
@@ -6,17 +6,17 @@ if(isset($_GET['id']) && isset($_GET['content']) && $_GET['content'] != "" && $_
     session_start();
     $id = $_GET['id'];
     $contentGET = htmlspecialchars($_GET['content']);
-    $userID=NULL;
+    $userIDA=NULL;
     if (isset($_COOKIE['benutzerdaten'])) {
     $username = explode("-", $_COOKIE['benutzerdaten'])[0];
     $password = explode("-", $_COOKIE['benutzerdaten'])[1];
-    $userID = $db->query("Select id FROM user WHERE password='$password' and username='$username'");
+    $userIDA = $db->query("Select id FROM user WHERE password='$password' and username='$username'");
     }
     $db = new PDO('mysql:host=localhost;dbname=news', 'root', '');
      
-    if(isset($userID)){
-    $userID = $userID->fetch(PDO::FETCH_ASSOC)[0]['id'];
-    echo $userID;
+    if(isset($userIDA)){
+    $userID = $userIDA->fetch(PDO::FETCH_ASSOC)[0]['id'];
+    echo "Deine USER ID " . $userID;
     $db->query("INSERT INTO comments (userID , annoucementID , created , content) VALUES ('$userID','$id','$date','$contentGET')");
     }  else {
     $db->query("INSERT INTO comments (annoucementID ,created ,content) VALUES ('$id','$date','$contentGET')");    
