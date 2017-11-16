aab83120c797b6826992681b83e464e89316012b
SevenOperation <mo.fernitz@hotmail.de>
2017-11-16 11:49:56 +0100

    p

--- Announcements/announcement.php
+++ Announcements/announcement.php
@@ -31,7 +31,7 @@ $extrascipt = "\r\n function showComments(id){".
     "\r\n if (request.readyState == 4 && request.status == 200){".
     "\r\n idCC.value = ''; ".
     "\r\n alert('Comment added');".
-    "\r\n }else if(request.status == 400){ ".
+    "\r\n }else if(request.readyState == 4 && request.status == 400){ ".
     "\r\n alert('Error: Comment field maybe emtpy' )".
     "\r\n }".
     "\r\n }".
