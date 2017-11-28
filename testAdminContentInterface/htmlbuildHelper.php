f091e4413aa0fb5ee19ee03a287bc84b0678ba93
SevenOperation <mo.fernitz@hotmail.de>
2017-11-28 13:37:39 +0100

    removed .php sowebsite will work again

--- testAdminContentInterface/htmlbuildHelper.php
+++ testAdminContentInterface/htmlbuildHelper.php
@@ -84,18 +84,18 @@ echo "<body style='background-image: url(/Logo_1.png);  background-size: cotain;
         "<div style='background-color: #24292e; padding-top: 12px; padding-bottom: 12px; line-height: 1.5 ;'>".
             "<div class='head' style='width: 52%; margin-left: auto; margin-right: auto; line-height: 1.5; font-size: 14px'>".
                 "<ul style='margin-top: 0; list-style: none; float: left; padding-left: 0; margin-bottom: 0'>".
-                    "<li><a href='/index.php'>Startseite</a></li>".
-                    "<li><a href='/News/news.php'>News</a></li>".
-                    "<li><a href='/games.php'>Games</a></li>".
-                    "<li><a href='/Announcements/announcement.php'>Ankündigungen</a></li>";
+                    "<li><a href='/index'>Startseite</a></li>".
+                    "<li><a href='/News/news'>News</a></li>".
+                    "<li><a href='/games'>Games</a></li>".
+                    "<li><a href='/Announcements/announcement'>Ankündigungen</a></li>";
                      if (isset($_COOKIE['benutzerdaten']) && isset($_SESSION["angemeldet" . $username]) && $_SESSION["angemeldet" . $username] == true) {
                      echo "</ul><ul style='margin: 0; list-style: none; float: right;'>
                      <li style='float: left'><a class='einloggenCss' onclick='userMenue()'>" . $username . "</a></li>
                      </ul></div></div>";
                         echo "<div style='width: 1080px; margin-left: auto; margin-right: auto; padding: 0;'>
                               <div style='position: absolute; z-index:2' id='login' class='drop2' align='center'>
-                              <p><a style='color:black;' href='/ControlInterface/controlinterface.php'>WebInterface</a></p>
-                              <p><a style='color:black;' href='/logout.php'>LogOut</a></p>
+                              <p><a style='color:black;' href='/ControlInterface/controlinterface'>WebInterface</a></p>
+                              <p><a style='color:black;' href='/logout'>LogOut</a></p>
                               </div></div>";
                     } else {
                         echo "</ul><ul style='margin: 0; list-style: none; float: right;'>
@@ -103,7 +103,7 @@ echo "<body style='background-image: url(/Logo_1.png);  background-size: cotain;
                     <li style='float: left'><a class='einloggenCss' onclick='registerUser()'>Registrieren</a></li>
                     </ul></div><div style='width: 52%; margin-left: auto; margin-right: auto;'>
                         <div id='login' style='width: 340px;' class='drop2' align='center'>
-                            <form action='/ControlInterface/datenueberpruefung.php' method='post'>
+                            <form action='/ControlInterface/datenueberpruefung' method='post'>
                                 <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
                                 <p><input class='input' id='password' name='password' type='password' placeholder='Password'/></p>
                                 <p><button type='submit'>Einloggen</button></p>
@@ -111,7 +111,7 @@ echo "<body style='background-image: url(/Logo_1.png);  background-size: cotain;
                         </div>
                     </div>
                     <div id='register' style='width: 340px;' class='drop2' align='center'>
-                        <form action='/ControlInterface/registrieren_datenueberpruefung.php' method='post'>
+                        <form action='/ControlInterface/registrieren_datenueberpruefung' method='post'>
                             <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
                             <p><input class='input' id='password' name='password' type='password' placeholder='Password'/></p>
                             <p><input class='input' id='passwordw' name='passwordw' type='password' placeholder='Password Wiederholen' required='required'/></p>
@@ -121,7 +121,7 @@ echo "<body style='background-image: url(/Logo_1.png);  background-size: cotain;
                     </div></div>";
                         echo "<div style='width: 960px; margin-left: auto; margin-right: auto;'>
                         <div id='login' style='width: 340px;' class='drop2' align='center'>
-                            <form action='/ControlInterface/datenueberpruefung.php' method='post'>
+                            <form action='/ControlInterface/datenueberpruefung' method='post'>
                                 <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
                                 <p><input class='input' id='password' name='password' type='password' placeholder='Password'/></p>
                                 <p><button type='submit'>Einloggen</button></p>
@@ -129,7 +129,7 @@ echo "<body style='background-image: url(/Logo_1.png);  background-size: cotain;
                         </div>
                     </div>
                     <div id='register' style='width: 340px;' class='drop2' align='center'>
-                        <form action='/ControlInterface/registrieren_datenueberpruefung.php' method='post'>
+                        <form action='/ControlInterface/registrieren_datenueberpruefung' method='post'>
                             <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
                             <p><input class='input' id='password' name='password' type='password' placeholder='Password'/></p>
                             <p><input class='input' id='passwordw' name='passwordw' type='password' placeholder='Password Wiederholen' required='required'/></p>
@@ -145,18 +145,18 @@ echo "<body style='background-image: url(/Logo_2.jpg);  background-size: cotain;
         "<div style='background-color: #24292e; padding-top: 12px; padding-bottom: 12px; line-height: 1.5 ;'>".
             "<div class='head' style='width: 960px; margin-left: auto; margin-right: auto; line-height: 1.5; font-size: 14px'>".
                 "<ul style='margin-top: 0; list-style: none; float: left; padding-left: 0; margin-bottom: 0'>".
-                    "<li><a href='/index.php'>Startseite</a></li>".
-                    "<li><a href='/News/news.php'>News</a></li>".
-                    "<li><a href='/games.php'>Games</a></li>".
-                    "<li><a href='/Announcements/announcement.php'>Ankündigungen</a></li>";
+                    "<li><a href='/index'>Startseite</a></li>".
+                    "<li><a href='/News/news'>News</a></li>".
+                    "<li><a href='/games'>Games</a></li>".
+                    "<li><a href='/Announcements/announcement'>Ankündigungen</a></li>";
                      if (isset($_COOKIE['benutzerdaten']) && isset($_SESSION["angemeldet" . $username]) && $_SESSION["angemeldet" . $username] == true) {
                      echo "</ul><ul style='margin: 0; list-style: none; float: right;'>
                      <li style='float: left'><a class='einloggenCss' onclick='userMenue()'>" . $username . "</a></li>
                      </ul></div></div>";
                         echo "<div style='width: 1080px; margin-left: auto; margin-right: auto; padding: 0;'>
                               <div style='position: absolute; z-index:2' id='login' class='drop2' align='center'>
-                              <p><a style='color:black;' href='/ControlInterface/controlinterface.php'>WebInterface</a></p>
-                              <p><a style='color:black;' href='/logout.php'>LogOut</a></p>
+                              <p><a style='color:black;' href='/ControlInterface/controlinterface'>WebInterface</a></p>
+                              <p><a style='color:black;' href='/logout'>LogOut</a></p>
                               </div></div>";
                     } else {
                         echo "</ul><ul style='margin: 0; list-style: none; float: right;'>
@@ -165,7 +165,7 @@ echo "<body style='background-image: url(/Logo_2.jpg);  background-size: cotain;
                     </ul></div></div>";
                         echo "<div style='width: 960px; margin-left: auto; margin-right: auto;'>
                         <div id='login' style='width: 340px;' class='drop2' align='center'>
-                            <form action='/ControlInterface/datenueberpruefung.php' method='post'>
+                            <form action='/ControlInterface/datenueberpruefung' method='post'>
                                 <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
                                 <p><input class='input' id='password' name='password' type='text' placeholder='Password'/></p>
                                 <p><button type='submit'>Einloggen</button></p>
@@ -173,7 +173,7 @@ echo "<body style='background-image: url(/Logo_2.jpg);  background-size: cotain;
                         </div>
                     </div>
                     <div id='register' style='width: 340px;' class='drop2' align='center'>
-                        <form action='/ControlInterface/registrieren_datenueberpruefung.php' method='post'>
+                        <form action='/ControlInterface/registrieren_datenueberpruefung' method='post'>
                             <p><input class='input' id='username' name='username' type='text' placeholder='Username'/></p>
                             <p><input class='input' id='password' name='password' type='text' placeholder='Password'/></p>
                             <p><input class='input' id='passwordw' name='passwordw' type='text' placeholder='Password Wiederholen' required='required'/></p>
