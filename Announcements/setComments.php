f3e59a345ef5016d3fba8106922faab82a25752c
SevenOperation <mo.fernitz@hotmail.de>
2018-01-17 23:19:50 +0100

    Better

--- Announcements/setComments.php
+++ Announcements/setComments.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../ControlInterface/datenueberpruefung.php';
 if(isset($_GET['id']) && isset($_GET['content']) && $_GET['content'] != "" && $_GET['id']!=""){
     $db = new PDO('mysql:host=localhost;dbname=users', 'root', '');
     $date = date("Y-m-d H:i:s");
@@ -7,7 +8,7 @@ if(isset($_GET['id']) && isset($_GET['content']) && $_GET['content'] != "" && $_
     $id = $_GET['id'];
     $contentGET = htmlspecialchars($_GET['content']);
     $userIDA=NULL;
-    if (isset($_COOKIE['benutzerdaten'])) {
+    if (checkLoggedIn()) {
     $username = explode("-", $_COOKIE['benutzerdaten'])[0];
     $password = explode("-", $_COOKIE['benutzerdaten'])[1];
     $userIDA = $db->query("Select id FROM user WHERE password='$password' and username='$username'");
--- testAdminContentInterface/auslesen.php
+++ testAdminContentInterface/auslesen.php
@@ -1,10 +1,21 @@
 <?php
+
+$resultN = "";
+$resultC = "1";
+$array = array();
+$position = 0;
 function auslesenHTML(){
-$db = new PDO("sqlite:/var/www/html/WebInterface/testAdminContentInterface/ts3server.sqlitedb");
+global $array , $resultC , $position, $resultN;
+$db = new PDO("sqlite:testAdminContentInterface/ts3server.sqlitedb");
 $ts_rooms = $db->query("Select * from channel_properties Where ident='channel_name' Order by id");
 $ts_names = $db->query("Select * from server_properties Where ident='virtualserver_name' AND id=1");
 foreach($ts_names as $ts_name){
 echo "<div style='color:white; width: 100%'>" . $ts_name["value"];
+}
+while($resultC !== false){
+$resultN = getClientInfo("client_nickname=",$position);
+$resultC = getClientInfo("cid=",$position);
+$array[$resultC] = $resultN;
 }
 #var_dump($db->errorInfo());
 foreach($ts_rooms as $room){
@@ -18,18 +29,39 @@ $buffer_new = "";
 for($i = 0; $i < 77; $i++){
 $buffer_new .= $buffer;
 }
-$r = "<div style='width:100%; margin: auto'>" . $buffer_new ;
+$r = "<div id='".$room['id']."' style='width:100%; margin: auto'>" . $buffer_new ;
 }
 elseif(strpos($room['value'], "spacer")){
-$r = "<div style='width:100%; margin: auto; height:19px'>";
+$r = "<div id='".$room['id']."' style='width:100%; margin: auto; height:19px'>";
 }
 else{
-$r = "<div style='width:100%; margin: auto'>" . explode("]",$room['value'])[1];
+$r = "<div id='".$room['id']."' style='width:100%; margin: auto'>" . explode("]",$room['value'])[1];
 }
 }
 else{
-$r = "<div style='width:100%; margin: auto'>" . $room["value"];
+$r = "<div id='".$room['id']."' style='width:100%; margin: auto'>" . $room["value"];
+if(isset($array[''.$room['id']])){
+$r = $r . $array[''.$room['id']];
+}
 }
 echo $r . "</div>";
 }
 }
+
+function getClientInfo($search,$start){
+global $position;
+$text = fread(fopen("testAdminContentInterface/clients", 'r'),filesize("testAdminContentInterface/clients"));
+$posa = strpos($text, $search , $start);
+if($posa === false){
+return false;
+}
+$posa += strlen($search);
+$posb = strpos($text, " ", $posa);
+$len = $posb - $posa;
+$position = $posb;
+return substr($text , $posa , $len);
+}
+#$text = "haha client_nickname=SevenOperation ";
+#echo strpos($text, "client_nickname=");
+#echo strpos($text, " ",5);
+#echo substr($text ,21 , 14);
