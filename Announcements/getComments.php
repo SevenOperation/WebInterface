d1be11674f573ec12ee057199879bdb5966b621f
SevenOperation <mo.fernitz@hotmail.de>
2018-01-18 01:14:38 +0100

    Profile picture is also now shown in the comment section

--- Announcements/getComments.php
+++ Announcements/getComments.php
@@ -8,15 +8,20 @@ if(isset($_GET['id'])){
     $db = new PDO('mysql:host=localhost;dbname=users', 'root', '');
     if($comments != FALSE && $comments->rowCount() != 0){
     foreach ($comments as $comment){
-        $creator = $db->query("Select username from user WHERE id=". $comment['userID'] ." ORDER BY id DESC");
+        $creator = $db->query("Select username , profilepicture from user WHERE id=". $comment['userID'] ." ORDER BY id DESC");
         $creatorS = "Unknown (Guest)";
+        $picture = "";
 	if(isset($creator) && $creator != false){
-        $creatorS = $creator->fetch(PDO::FETCH_ASSOC)['username'];
+        foreach ($creator as $data){
+        $creatorS = $data['username'];
+        $picture = $data['profilepicture'];
         }
+        }
+        echo $creator->fetch(PDO::FETCH_ASSOC)['profilepicture'];
         if(!isset($creatorS)) $creatorS="Unknown (Guest)";
         echo "<div name='".$comment['annoucementID']."C'style='background-color: white;'>\r\n".
               "\r\n <p style='font-size:16px'>".$creatorS."</p>".
-              "\r\n <img src='' height='48' width='48' style='position: absolute'></img>".
+              "\r\n <img src='". $picture ."' height='48' width='48' style='position: absolute'></img>".
               "<p style='margin-left: 50px; margin-right: 20px; font-size:16px; height: 96px; border-color: black; border-width: 1px; border-style: solid; box-sizing: border-box'>".nl2br(htmlspecialchars($comment['content']))."</p>\r\n".
               "<p style='font-size:16px; margin-top: 10px'>".$comment['created']."</p>\r\n</div>";
     }
