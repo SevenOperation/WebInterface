1a7faff5871014615bab1f881e50332e9ee8eb5a
SevenOperation <mo.fernitz@hotmail.de>
2018-03-22 16:08:20 +0100

    Finnaly got the teamspeak 3 room overview done

--- AdminContentInterface/auslesen.php
+++ AdminContentInterface/auslesen.php
@@ -7,7 +7,7 @@ $position = 0;
 function auslesenHTML(){
 global $array , $resultC , $position, $resultN, $path;
 $db = new PDO("sqlite:".$path."/ts3server.sqlitedb");
-$ts_rooms = $db->query("Select value from channel_properties Where ident='channel_name' Order by case when ident = 'channel_order' then value end");
+$ts_rooms = $db->query("Select max(case when ident='channel_order' then value end) value_a ,max(case when ident='channel_name' then value end) value_b from channel_properties group by id order by cast(value_a as int)");
 var_dump($db->errorInfo());
 $ts_names = $db->query("Select * from server_properties Where ident='virtualserver_name' AND id=1");
 foreach($ts_names as $ts_name){
@@ -19,30 +19,36 @@ $resultC = getClientInfo("cid=",$position);
 $array[$resultC] = $resultN;
 }
 #var_dump($db->errorInfo());
+/**$sorted_array = array();
+$ts_room = $ts_rooms->fetch(PDO::FETCH_BOTH);
+for ($zahl = 0; $zahl < sizeof($ts_rooms) ; $zahl++){
+ //$sorted_array[$ts_room["value"]] = $ts_room['value'];
+ echo "".$ts_room[$zahl];
+}**/
 foreach($ts_rooms as $room){
 $r = "";
-if(strpos($room["value"],"]")){
- if(strpos($room["value"],"cspacer")){
- $r = "<div style='width:50%; margin: auto; text-align: center'>" . explode("]",$room['value'])[1];
- }elseif(strpos($room["value"],"*spacer")){
-$buffer = explode("]",$room['value'])[1];
+if(strpos($room["value_b"],"]")){
+ if(strpos($room["value_b"],"cspacer")){
+ $r = "<div style='width:50%; margin: auto; text-align: center'>" . explode("]",$room['value_b'])[1];
+ }elseif(strpos($room["value_b"],"*spacer")){
+$buffer = explode("]",$room['value_b'])[1];
 $buffer_new = "";
 for($i = 0; $i < 77; $i++){
 $buffer_new .= $buffer;
 }
-$r = "<div id='".$room['id']."' style='width:100%; margin: auto'>" . $buffer_new ;
+$r = "<div id='".$room['value_a']."' style='width:100%; margin: auto'>" . $buffer_new ;
 }
-elseif(strpos($room['value'], "spacer")){
-$r = "<div id='".$room['id']."' style='width:100%; margin: auto; height:19px'>";
-}
-else{
-$r = "<div id='".$room['id']."' style='width:100%; margin: auto'>" . explode("]",$room['value'])[1];
-}
+elseif(strpos($room['value_a'], "spacer")){
+$r = "<div id='".$room['value_a']."' style='width:100%; margin: auto; height:19px'>";
 }
 else{
-$r = "<div id='".$room['id']."' style='width:100%; margin: auto'>" . $room["value"];
-if(isset($array[''.$room['id']])){
-$r = $r . $array[''.$room['id']];
+$r = "<div id='".$room['value_a']."' style='width:100%; margin: auto'>" . explode("]",$room['value_b'])[1];
+}
+}
+else{
+$r = "<div id='".$room['value_a']."' style='width:100%; margin: auto'>" . $room["value_b"];
+if(isset($array[''.$room['value_a']])){
+$r = $r . $array[''.$room['value_a']];
 }
 }
 echo $r . "</div>";
