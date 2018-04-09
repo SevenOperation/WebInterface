ff45dbd7927381c1726aaf38fa0ec65521ef7ff8
SevenOperation <mo.fernitz@hotmail.de>
2018-04-09 14:48:05 +0200

    Added python script for newsletter emails and also html layout for the newsletter email

---
+++ AdminContentInterface/newsletter.py
@@ -0,0 +1,16 @@
+#!/bin/python
+import smtplib
+import MySQLdb
+import sys
+
+sender = sys.argv[0]
+mail_domain = sys.argv[1]
+data = open("layout.html",'r')
+server = smtplib.SMTP("127.0.0.1",25,"mail_domain",10)
+server.set_debuglevel(1)
+db = MySQLdb.connect(host='127.0.0.1',db='users')
+cnx = db.cursor()
+daten = cnx.execute('select email from user where newsletter=1')
+for row in cnx.fetchall():
+	server.sendmail(sender,row[0],data.read())
+server.quit()
